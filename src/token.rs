0a4ef92ad30091846bd68d91ddc7d47a6212032e
hayas1 <h4ystack@gmail.com>
2024-02-17 03:57:57 +0000

    iter with position

--- src/token.rs
+++ src/token.rs
@@ -12,11 +12,12 @@ impl<R> Tokenizer<R>
 where
     R: io::Read,
 {
-    pub fn pos(&self) -> (usize, usize) {
+    pub fn pos(&self) -> Position {
         self.iter.pos()
     }
 }
 
+pub type Position = (usize, usize);
 pub struct RowColIterator<I> {
     iter: I,
     row: usize,
@@ -27,7 +28,7 @@ impl<I> RowColIterator<I> {
         RowColIterator { iter, row: 0, col: 0 }
     }
 
-    pub fn pos(&self) -> (usize, usize) {
+    pub fn pos(&self) -> Position {
         (self.row, self.col)
     }
 }
@@ -35,10 +36,11 @@ impl<I> Iterator for RowColIterator<I>
 where
     I: Iterator<Item = io::Result<u8>>,
 {
-    type Item = io::Result<u8>;
+    type Item = (Position, I::Item);
 
     fn next(&mut self) -> Option<Self::Item> {
         self.iter.next().map(|c| {
+            let pos = self.pos();
             match c {
                 Ok(b'\n') => {
                     self.row += 1;
@@ -47,7 +49,7 @@ where
                 Ok(_) => self.col += 1,
                 Err(_) => {}
             }
-            c
+            (pos, c)
         })
     }
 }
@@ -65,49 +67,49 @@ mod tests {
     fn behavior_row_col_iterator() {
         let reader = BufReader::new(File::open("tests/data/list.json").unwrap());
         let mut iter = RowColIterator::new(reader.bytes());
-        assert!(matches!((iter.pos(), iter.next()), ((0, 0), Some(Ok(b'[')))));
-        assert!(matches!((iter.pos(), iter.next()), ((0, 1), Some(Ok(b'\n')))));
+        assert!(matches!(iter.next(), Some(((0, 0), Ok(b'[')))));
+        assert!(matches!(iter.next(), Some(((0, 1), Ok(b'\n')))));
 
-        assert!(matches!((iter.pos(), iter.next()), ((1, 0), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 1), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 2), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 3), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 4), Some(Ok(b'"')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 5), Some(Ok(b'f')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 6), Some(Ok(b'o')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 7), Some(Ok(b'o')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 8), Some(Ok(b'"')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 9), Some(Ok(b',')))));
-        assert!(matches!((iter.pos(), iter.next()), ((1, 10), Some(Ok(b'\n')))));
+        assert!(matches!(iter.next(), Some(((1, 0), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((1, 1), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((1, 2), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((1, 3), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((1, 4), Ok(b'"')))));
+        assert!(matches!(iter.next(), Some(((1, 5), Ok(b'f')))));
+        assert!(matches!(iter.next(), Some(((1, 6), Ok(b'o')))));
+        assert!(matches!(iter.next(), Some(((1, 7), Ok(b'o')))));
+        assert!(matches!(iter.next(), Some(((1, 8), Ok(b'"')))));
+        assert!(matches!(iter.next(), Some(((1, 9), Ok(b',')))));
+        assert!(matches!(iter.next(), Some(((1, 10), Ok(b'\n')))));
 
-        assert!(matches!((iter.pos(), iter.next()), ((2, 0), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 1), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 2), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 3), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 4), Some(Ok(b'"')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 5), Some(Ok(b'b')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 6), Some(Ok(b'a')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 7), Some(Ok(b'r')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 8), Some(Ok(b'"')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 9), Some(Ok(b',')))));
-        assert!(matches!((iter.pos(), iter.next()), ((2, 10), Some(Ok(b'\n')))));
+        assert!(matches!(iter.next(), Some(((2, 0), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((2, 1), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((2, 2), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((2, 3), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((2, 4), Ok(b'"')))));
+        assert!(matches!(iter.next(), Some(((2, 5), Ok(b'b')))));
+        assert!(matches!(iter.next(), Some(((2, 6), Ok(b'a')))));
+        assert!(matches!(iter.next(), Some(((2, 7), Ok(b'r')))));
+        assert!(matches!(iter.next(), Some(((2, 8), Ok(b'"')))));
+        assert!(matches!(iter.next(), Some(((2, 9), Ok(b',')))));
+        assert!(matches!(iter.next(), Some(((2, 10), Ok(b'\n')))));
 
-        assert!(matches!((iter.pos(), iter.next()), ((3, 0), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 1), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 2), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 3), Some(Ok(b' ')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 4), Some(Ok(b'"')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 5), Some(Ok(b'b')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 6), Some(Ok(b'a')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 7), Some(Ok(b'z')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 8), Some(Ok(b'"')))));
-        assert!(matches!((iter.pos(), iter.next()), ((3, 9), Some(Ok(b'\n')))));
+        assert!(matches!(iter.next(), Some(((3, 0), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((3, 1), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((3, 2), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((3, 3), Ok(b' ')))));
+        assert!(matches!(iter.next(), Some(((3, 4), Ok(b'"')))));
+        assert!(matches!(iter.next(), Some(((3, 5), Ok(b'b')))));
+        assert!(matches!(iter.next(), Some(((3, 6), Ok(b'a')))));
+        assert!(matches!(iter.next(), Some(((3, 7), Ok(b'z')))));
+        assert!(matches!(iter.next(), Some(((3, 8), Ok(b'"')))));
+        assert!(matches!(iter.next(), Some(((3, 9), Ok(b'\n')))));
 
-        assert!(matches!((iter.pos(), iter.next()), ((4, 0), Some(Ok(b']')))));
-        assert!(matches!((iter.pos(), iter.next()), ((4, 1), Some(Ok(b'\n')))));
+        assert!(matches!(iter.next(), Some(((4, 0), Ok(b']')))));
+        assert!(matches!(iter.next(), Some(((4, 1), Ok(b'\n')))));
 
-        assert!(matches!((iter.pos(), iter.next()), ((5, 0), None)));
-        assert!(matches!((iter.pos(), iter.next()), ((5, 0), None)));
-        assert!(matches!((iter.pos(), iter.next()), ((5, 0), None)));
+        assert!(matches!(iter.next(), None));
+        assert!(matches!(iter.next(), None));
+        assert!(matches!(iter.next(), None));
     }
 }
