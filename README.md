638f8ae77b7c4255d853266ca0f7cb1243f50082
hayas1 <h4ystack@gmail.com>
2024-03-01 13:14:48 +0000

    doc

---
+++ README.md
@@ -0,0 +1,45 @@
+# json-with-comments
+
+JSON with comments parser for Rust.
+
+## Usage
+in `Cargo.toml`
+```toml
+[dependencies]
+json_with_comments = { git = "https://github.com/hayas1/json-with-comments" }
+```
+
+## Parse JSONC as typed struct
+Any type that implements [`serde::Deserialize`] can be deserialized from JSONC text.
+```rust
+#[derive(serde::Deserialize)]
+struct Person<'a> {
+    name: &'a str,
+    address: Address<'a>,
+}
+#[derive(serde::Deserialize)]
+struct Address<'a> {
+    street: &'a str,
+    number: u32,
+}
+
+let json = r#"{
+    "name": "John Doe", // John Doe is a fictional character
+    "address": {
+        "street": "Main",
+        "number": 42, /* trailing comma */
+    },
+}"#;
+
+let data: Person = json_with_comments::from_str(json).unwrap();
+assert!(matches!(
+    data,
+    Person {
+        name: "John Doe",
+        address: Address { street: "Main", number: 42 }
+    }
+));
+```
+
+## Performance
+// TODO
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+//! JSON with comments parser for Rust.
+//!
+//! # Usage
+//! in `Cargo.toml`
+//! ```toml
+//! [dependencies]
+//! json_with_comments = { git = "https://github.com/hayas1/json-with-comments" }
+//! ```
+//!
+//! # Parse JSONC as typed struct
+//! Any type that implements [`serde::Deserialize`] can be deserialized from JSONC text.
+//! ```rust
+//! #[derive(serde::Deserialize)]
+//! struct Person<'a> {
+//!     name: &'a str,
+//!     address: Address<'a>,
+//! }
+//! #[derive(serde::Deserialize)]
+//! struct Address<'a> {
+//!     street: &'a str,
+//!     number: u32,
+//! }
+//!
+//! let json = r#"{
+//!     "name": "John Doe", // John Doe is a fictional character
+//!     "address": {
+//!         "street": "Main",
+//!         "number": 42, /* trailing comma */
+//!     },
+//! }"#;
+//!
+//! let data: Person = json_with_comments::from_str(json).unwrap();
+//! assert!(matches!(
+//!     data,
+//!     Person {
+//!         name: "John Doe",
+//!         address: Address { street: "Main", number: 42 }
+//!     }
+//! ));
+//! ```
+//!
+//! # Performance
+//! // TODO
+
 pub mod de;
 pub mod error;
 pub mod value;
