c06d7a195407f0c2eeabf89781dbb138a1642791
hayas1 <h4ystack@gmail.com>
2024-03-07 14:49:22 +0000

    methods

--- src/value.rs
+++ src/value.rs
@@ -13,3 +13,41 @@ pub enum JsoncValue<I, F> {
     String(String),
     Number(number::NumberValue<I, F>),
 }
+
+impl<I, F> Default for JsoncValue<I, F> {
+    fn default() -> Self {
+        Self::Null
+    }
+}
+impl<I: num::FromPrimitive, F: num::FromPrimitive> std::str::FromStr for JsoncValue<I, F> {
+    type Err = crate::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        crate::from_str(s)
+    }
+}
+impl<I, F> JsoncValue<I, F> {
+    /// TODO doc
+    pub fn query(&self, query: &str) -> Option<&JsoncValue<I, F>> {
+        // TODO better implement, tests
+        query.split('.').try_fold(self, |value, key| match value {
+            JsoncValue::Object(map) => map.get(key),
+            JsoncValue::Array(vec) => vec.get(key.parse::<usize>().ok()?),
+            _ => None,
+        })
+    }
+    /// TODO doc
+    pub fn take(&mut self) -> Self {
+        std::mem::take(self)
+    }
+}
+
+// TODO implement Deserializer for JsoncValue
+// TODO doc
+// pub fn from_value<T, I, F>(value: JsoncValue<I, F>) -> crate::Result<T>
+// where
+//     T: serde::de::DeserializeOwned,
+//     I: num::FromPrimitive,
+//     F: num::FromPrimitive,
+// {
+//     T::deserialize(value)
+// }
