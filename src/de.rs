b0d13509c142fb6d57883efac2cbbe76f6ee03f3
hayas1 <h4ystack@gmail.com>
2024-03-17 01:04:24 +0000

    move test deserialize ignored

--- src/de.rs
+++ src/de.rs
@@ -378,4 +378,34 @@ mod tests {
             Jsonc { name: "JSON with comments", keywords: vec!["JSON", "JSONC", "trailing comma"] }
         );
     }
+
+    #[test]
+    fn test_deserialize_ignored() {
+        #[derive(serde::Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        struct Setting {
+            name: &'static str,
+            image: Option<&'static str>,
+            remote_user: Option<&'static str>,
+            mounts: Option<Vec<&'static str>>,
+        }
+        let target = r#"{
+            "name": "Debian",
+            "image": "mcr.microsoft.com/vscode/devcontainers/base:0-bullseye",
+            "remoteUser": "vscode",
+            "mounts": null,
+            "customizations": {}, // this field is not defined in struct
+            "features": {}, /* this field is not defined in struct */
+        }"#;
+        let setting = from_str::<Setting>(target).unwrap();
+        assert!(matches!(
+            setting,
+            Setting {
+                name: "Debian",
+                image: Some("mcr.microsoft.com/vscode/devcontainers/base:0-bullseye"),
+                remote_user: Some("vscode"),
+                mounts: None
+            }
+        ));
+    }
 }
--- tests/integration/spec.rs
+++ tests/integration/spec.rs
@@ -66,38 +66,6 @@ fn test_deserialize_literal_matchable() {
 }
 
 #[test]
-fn test_deserialize_ignored() {
-    #[derive(Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    struct Setting {
-        name: &'static str,
-        image: Option<&'static str>,
-        remote_user: Option<&'static str>,
-        mounts: Option<Vec<&'static str>>,
-    }
-    let target = r#"
-        {
-            "name": "Debian",
-            "image": "mcr.microsoft.com/vscode/devcontainers/base:0-bullseye",
-            "remoteUser": "vscode",
-            "mounts": null,
-            "customizations": {}, // this field is not defined in struct
-            "features": {}, /* this field is not defined in struct */
-        }
-    "#;
-    let setting = from_str::<Setting>(target).unwrap();
-    assert!(matches!(
-        setting,
-        Setting {
-            name: "Debian",
-            image: Some("mcr.microsoft.com/vscode/devcontainers/base:0-bullseye"),
-            remote_user: Some("vscode"),
-            mounts: None
-        }
-    ));
-}
-
-#[test]
 fn test_deserialize_enum() {
     #[derive(Deserialize, PartialEq, Debug)]
     struct Server {
