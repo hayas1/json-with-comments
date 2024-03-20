86dbae6728cce5eaca1f0f70c18709367e40e856
hayas1 <h4ystack@gmail.com>
2024-03-20 04:48:26 +0000

    refactor `FromNumberBuilder` for `Number`

--- src/value/number.rs
+++ src/value/number.rs
@@ -5,12 +5,16 @@ pub enum Number<I, F> {
     Integer(I),
     Float(F),
 }
-impl FromNumberBuilder for Number<i64, f64> {
+
+impl<I: FromNumberBuilder, F: FromNumberBuilder> FromNumberBuilder for Number<I, F>
+where
+    crate::Error: From<I::Err> + From<F::Err>,
+{
     type Err = crate::Error;
     fn from_number_builder(builder: NumberBuilder) -> Result<Self, Self::Err> {
         match builder.ty() {
-            Number::Integer(()) => Ok(Number::Integer(i64::from_number_builder(builder)?)),
-            Number::Float(()) => Ok(Number::Float(f64::from_number_builder(builder)?)),
+            Number::Integer(()) => Ok(Number::Integer(I::from_number_builder(builder)?)),
+            Number::Float(()) => Ok(Number::Float(F::from_number_builder(builder)?)),
         }
     }
 }
