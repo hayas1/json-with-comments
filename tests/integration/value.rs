607c88af6b8345b4f4dcad53bd5e49d544984717
hayas1 <h4ystack@gmail.com>
2024-03-28 15:24:01 +0000

    roundtrip string <=> value

--- tests/integration/value.rs
+++ tests/integration/value.rs
@@ -1,64 +1,80 @@
 use json_with_comments::{
-    from_str,
-    value::{number::Number, JsoncValue, MapImpl},
+    from_str, jsonc_generics, to_string,
+    value::{number::Number, JsoncValue},
     Value,
 };
 
 #[test]
-fn test_deserialize_null_as_value() {
+fn test_deserialize_serialize_null_as_value() {
     let target = r#"null"#;
     let value: Value = from_str(target).unwrap();
     assert_eq!(value, JsoncValue::Null);
+
+    let null = to_string(value).unwrap();
+    assert_eq!(null, target);
 }
 
 #[test]
-fn test_deserialize_bool_as_value() {
+fn test_deserialize_serialize_bool_as_value() {
     let target = r#"true"#;
     let value: Value = from_str(target).unwrap();
     assert_eq!(value, JsoncValue::Bool(true));
+
+    let tru = to_string(value).unwrap();
+    assert_eq!(tru, target);
 }
 
 #[test]
-fn test_deserialize_number_as_value() {
+fn test_deserialize_serialize_number_as_value() {
     let target = r#"9"#;
     let value: Value = from_str(target).unwrap();
     assert_eq!(value, JsoncValue::Number(Number::Integer(9)));
+
+    let num = to_string(value).unwrap();
+    assert_eq!(num, target);
 }
 
 #[test]
-fn test_deserialize_string_as_value() {
+fn test_deserialize_serialize_string_as_value() {
     let target = r#""string""#;
     let value: Value = from_str(target).unwrap();
     assert_eq!(value, JsoncValue::String("string".to_string()));
+
+    let string = to_string(value).unwrap();
+    assert_eq!(string, target);
 }
 
 #[test]
-fn test_deserialize_array_as_value() {
-    let target = r#"[null, true, "false", 10]"#;
+fn test_deserialize_serialize_array_as_value() {
+    let target = r#"[null,true,"false",10]"#;
     let value: Value = from_str(target).unwrap();
-    assert_eq!(
-        value,
-        JsoncValue::Array(vec![
-            JsoncValue::Null,
-            JsoncValue::Bool(true),
-            JsoncValue::String("false".to_string()),
-            JsoncValue::Number(Number::Integer(10)),
-        ])
-    );
+    assert_eq!(value, jsonc_generics!([null, true, "false", 10]));
+
+    let array = to_string(value).unwrap();
+    assert_eq!(array, target);
 }
 
 #[test]
-fn test_deserialize_object_as_value() {
-    let target = r#"{"null": null, "bool": true, "str": "false", "number": 10000000000, "float": 1.5}"#;
-    let value: JsoncValue<i128, f32> = from_str(target).unwrap();
+fn test_deserialize_serialize_object_as_value() {
+    let target = r#"{"null":null,"number":10000000000,"float":1.5}"#;
+    let value: JsoncValue<i64, f32> = from_str(target).unwrap();
     assert_eq!(
         value,
-        JsoncValue::Object(MapImpl::from([
-            ("null".to_string(), JsoncValue::Null),
-            ("bool".to_string(), JsoncValue::Bool(true)),
-            ("str".to_string(), JsoncValue::String("false".to_string())),
-            ("number".to_string(), JsoncValue::Number(Number::Integer(10000000000))),
-            ("float".to_string(), JsoncValue::Number(Number::Float(1.5))),
-        ]))
+        jsonc_generics!({
+            "null": null,
+            "number": 10000000000,
+            "float": 1.5
+        })
     );
+
+    let object = to_string(value).unwrap();
+    assert!([
+        target,
+        r#"{"null":null,"float":1.5,"number":10000000000}"#,
+        r#"{"number":10000000000,"null":null,"float":1.5}"#,
+        r#"{"number":10000000000,"float":1.5,"null":null}"#,
+        r#"{"float":1.5,"number":10000000000,"null":null}"#,
+        r#"{"float":1.5,"null":null,"number":10000000000}"#,
+    ]
+    .contains(&&object[..]));
 }
